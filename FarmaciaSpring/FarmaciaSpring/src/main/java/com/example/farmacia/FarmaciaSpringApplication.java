51073084130929144441a9db485d24689c12eeb1
Pedro <pedrocsca@gmail.com>
2022-08-02 16:33:25 -0300

    adicionando atv farmacia spring

---
+++ FarmaciaSpring/FarmaciaSpring/src/main/java/com/example/farmacia/FarmaciaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.farmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaSpringApplication.class, args);
+	}
+
+}
