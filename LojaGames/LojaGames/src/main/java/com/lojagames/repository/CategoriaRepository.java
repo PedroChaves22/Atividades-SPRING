16f35bc199e5437d97627e200f15bb997a8f9081
Pedro <pedrocsca@gmail.com>
2022-08-05 13:36:12 -0300

    adicionando loja de games

---
+++ LojaGames/LojaGames/src/main/java/com/lojagames/repository/CategoriaRepository.java
@@ -0,0 +1,15 @@
+package com.lojagames.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lojagames.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	
+	public List<Categoria> findAllByTipoContainingIgnoreCase(String tipo);
+
+}
+
+
