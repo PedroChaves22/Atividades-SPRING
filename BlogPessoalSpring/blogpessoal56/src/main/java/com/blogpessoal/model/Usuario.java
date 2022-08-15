012e4926d3eb468dab0746fa03745954c117fe1b
Pedro <pedrocsca@gmail.com>
2022-08-15 18:43:40 -0300

    atualizando Blog pessoal

--- BlogPessoalSpring/blogpessoal56/src/main/java/com/blogpessoal/model/Usuario.java
+++ BlogPessoalSpring/blogpessoal56/src/main/java/com/blogpessoal/model/Usuario.java
@@ -36,6 +36,18 @@ private String senha;
  @JsonIgnoreProperties("usuario")
 private List<Postagem> postagem;
 
+ 
+public Usuario(Long id, String nome, String foto, String usuario,String senha) {
+	super();
+	this.id = id;
+	this.nome = nome;
+	this.foto = foto;
+	this.usuario = usuario;
+	this.senha = senha;
+}
+
+public Usuario() { }
+
 /* Insira os Getters and Setters */
 public Long getId() {
 return id;
